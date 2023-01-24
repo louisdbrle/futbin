5ea4ae3f01df562b8ac189b1037dfd831e508479
Alexandre-Da-Cunha-Guiborat <alexandredacunhaguiborat.pro@gmail.com>
2023-01-24 14:12:01 +0100

    install script

---
+++ install_SDL2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+echo "This script runs sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev, in order to install sdl2 libraries required to build the game. DO NOT TYPE YOUR PASSWORD IF YOU DON'T WANT THIS SCRIPT TO RUN"
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev
